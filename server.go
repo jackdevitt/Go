adf831636a16043ca28de22430f816f80044b07f
jackdevitt <jacktdevitt@gmail.com>
2023-06-28 10:21:48 -0400

    Added Delete/Patched Post

--- server.go
+++ server.go
@@ -44,34 +44,47 @@ func checkHealth(c *gin.Context) {
 }
 
 func addItem(c *gin.Context) {
+	//Establish database connection
 	sqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 	db, err := sql.Open("postgres", sqlInfo)
 	if err != nil {
+		//Send error on failed connection
 		response := Response{Action: "SQL", Sucessful: false, Context: err.Error()}
 		c.IndentedJSON(http.StatusOK, response)
 	}
 	defer db.Close()
 	db.Ping()
 
+	//Get info from parameters
 	_, foundCompletion := c.GetQuery("rawCompleted")
 	_, foundPriority := c.GetQuery("rawPriority")
 
 	length, sizeerr := db.Query("SELECT * FROM items")
-	counter := 1
+	highest := 1
+	//Find ID for next entry
 	for length.Next() {
-		counter++
+		var entry Item
+		length.Scan(&entry.ID, &entry.Name, &entry.Desc, &entry.TopPriority, &entry.Completed)
+		if (entry.ID > highest) {
+			highest = entry.ID
+		}
 	}
 	if sizeerr != nil {
 		response := Response{Action: "SQL", Sucessful: false, Context: sizeerr.Error()}
 		c.IndentedJSON(http.StatusOK, response)
 	}
+	//SQL command
 	insertCMD := `INSERT INTO items (ID, Name, Description, Priority, Completed)
 	VALUES ($1, $2, $3, $4, $5)`
-	_, cmderr := db.Exec(insertCMD, counter, c.DefaultQuery("rawName", "Untitled"), c.DefaultQuery("rawDesc", "No Description"), foundPriority, foundCompletion)
+	_, cmderr := db.Exec(insertCMD, highest + 1, c.DefaultQuery("rawName", "Untitled"), c.DefaultQuery("rawDesc", "No Description"), foundPriority, foundCompletion)
 	if cmderr != nil {
 		response := Response{Action: "SQL", Sucessful: false, Context: cmderr.Error()}
+		c.IndentedJSON(http.StatusOK, response)
+	} else {
+		//Send message for successful POST method
+		response := Response{Action: "Post", Sucessful: true, Context: "Posted without error"}
 		c.IndentedJSON(http.StatusOK, response)
 	}
 }
@@ -176,38 +189,36 @@ func getItems(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, collection)
 }
 
-/*func getItems(c *gin.Context) {
-	var collection Collection
-	var str = readFile("list.json")
-	json.Unmarshal([]byte(str), &collection)
-	filter, status := c.GetQuery("rawFilter")
-	if !status {
-		c.IndentedJSON(http.StatusOK, collection)
-	} else {
-		count := 0
-		for i := 0; i < len(collection.Items); i++ {
-			if strings.EqualFold(collection.Items[i].Name, filter) {
-				count++
-			}
-		}
-		if count != 0 {
-			index := 0
-			newArr := make([]Item, count)
-			for i := 0; i < len(collection.Items); i++ {
-				if strings.EqualFold(collection.Items[i].Name, filter) {
-					newArr[index] = collection.Items[i]
-					index++
-				}
-			}
-			collection.Items = newArr
-			c.IndentedJSON(http.StatusOK, collection.Items)
-		} else {
-			response := Response{Action: "Get", Sucessful: false, Context: "No queries match filter"}
-			c.IndentedJSON(http.StatusOK, response)
-		}
+//Handler for /removeItem endpoint with DELETE method
+func removeItem(c *gin.Context) {
+	//Make initial connection with database
+	sqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+	"password=%s dbname=%s sslmode=disable",
+	host, port, user, password, dbname)
+	db, err := sql.Open("postgres", sqlInfo)
+	if err != nil {
+		//Return an error if database is not active
+		response := Response{Action: "SQL", Sucessful: false, Context: err.Error()}
+		c.IndentedJSON(http.StatusOK, response)
 	}
-}*/
+	db.Ping()
+	//Get value from Path
+	identifier := c.Param("id")
+	//SQL command
+	_, err = db.Query(
+		`DELETE FROM items
+		WHERE id = $1;`, identifier)
+	if err != nil {
+		response := Response{Action: "SQL", Sucessful: false, Context: err.Error()}
+		c.IndentedJSON(http.StatusOK, response)
+	} else {
+		//Send message for successful POST method
+		response := Response{Action: "Delete", Sucessful: true, Context: "Removed without error"}
+		c.IndentedJSON(http.StatusOK, response)
+	}
+}
 
+/*
 func removeItem(c *gin.Context) {
 	_, status := c.GetQuery("rawID")
 	index, _ := strconv.Atoi(c.Query("rawID"))
@@ -235,6 +246,7 @@ func removeItem(c *gin.Context) {
 		}
 	}
 }
+*/
 
 func replaceItem(c *gin.Context) {
 	_, status := c.GetQuery("rawID")
@@ -333,7 +345,7 @@ func main() {
 	router.GET("/health", checkHealth)
 	router.GET("/SQL", sqlf)
 	router.POST("/addItem", addItem)
-	router.DELETE("/removeItem", removeItem)
+	router.DELETE("/removeItem/:id", removeItem)
 	router.GET("/getItems/:id", getItems)
 	router.GET("/getItems", getItems)
 	router.PATCH("/replaceItem", replaceItem)
